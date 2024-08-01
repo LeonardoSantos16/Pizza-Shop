7bb659862fe765fd0d7c0f0aa8311d791152d9f2
leonardo <leodsmf@gmail.com>
2024-08-01 12:32:48 -0300

    criação da pagina pedidos e os componentes de paginação, filtro e detalhes de pedido

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,21 +3,36 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth.tsx'
 import { Dashboard } from './pages/app/Dashboard'
+import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in.tsx'
 import { SignUp } from './pages/auth/sign-up.tsx'
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    children: [{ path: '/', element: <Dashboard /> }],
+    children: [
+      {
+        path: '/',
+        element: <Dashboard />,
+      },
+      {
+        path: '/orders',
+        element: <Orders />,
+      },
+    ],
   },
-
   {
     path: '/',
     element: <AuthLayout />,
     children: [
-      { path: '/sign-in', element: <SignIn /> },
-      { path: '/sign-up', element: <SignUp /> },
+      {
+        path: '/sign-in',
+        element: <SignIn />,
+      },
+      {
+        path: '/sign-up',
+        element: <SignUp />,
+      },
     ],
   },
 ])
