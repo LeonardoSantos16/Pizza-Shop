abbf062e9fc3269842c694771f170ece4b6c4976
leonardo <leodsmf@gmail.com>
2024-08-01 10:42:03 -0300

    página cadastro e login, notificações toast e react-hook-form commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { RouterProvider } from 'react-router-dom'
 import { Button } from './components/ui/button'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import './global.css'
+import { Toaster } from 'sonner'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | pizza.shop" />
+
+      <Toaster richColors />
 
       <RouterProvider router={router} />
     </HelmetProvider>
